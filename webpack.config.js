e0aaad83e848740af69e918379fdccc5a5f87f1f
Aaron Cronin <cronin@include.cat>
2015-06-27 16:11:41 +0200

    Remove source map

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,21 @@
 module.exports = {
-	entry: './source/app.ts',
-	
-	resolve: {
-    	extensions: ['', '.ts', '.webpack.js', '.web.js', '.js']
-  	},
-	  
-	output: {
-		path: './target',
-		filename: 'bundle.js'
-	},
-	
-	devtool: 'eval-source-map', //init compile fast, recompile also very fast
-	
-	module: {
-		loaders: [
-			{ 	
-				test: /\.ts$/,
-				loader: 'awesome-typescript-loader'
-			}
-		]
-	}
+  entry: './source/app.ts',
+  
+  resolve: {
+      extensions: ['', '.ts', '.webpack.js', '.web.js', '.js']
+    },
+    
+  output: {
+    path: './target',
+    filename: 'bundle.js'
+  },
+  
+  module: {
+    loaders: [
+      {   
+        test: /\.ts$/,
+        loader: 'awesome-typescript-loader'
+      }
+    ]
+  }
 };
